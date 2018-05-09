3a28e1c050ead15878975b2b89dd1eab592af56a
David Scialom Brima <davidscialom.brima@post.idc.ac.il>
2018-05-09 18:24:19 +0300

    Fixed Car implementation and Added Fuel-Based Cars

--- Ex03.GarageLogic/Car.cs
+++ Ex03.GarageLogic/Car.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Program;
 
 namespace Ex03.GarageLogic
 {
-    abstract class Car 
+    abstract class Car : Vehicle
     {
-        private enum Color
-        {
-            Gray = 1,
-            Blue = 2,
-            White = 3,
-            Black = 4
-        }
-        private enum NumOfDoors
-        {
-            Two = 2,
-            Three = 3,
-            Four = 4,
-            Five = 5
-        }
+        eTypeColor m_TypeOfColor;
+        eNumOfDoors m_NumOfDoors;
+        
+    }
+     enum eTypeColor
+    {
+        Gray = 1,
+        Blue = 2,
+        White = 3,
+        Black = 4
+    }
+     enum eNumOfDoors
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5
     }
 }
---
+++ Ex03.GarageLogic/Fuel-Based Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    class Fuel_Based_Car : Car , IFuelable
+    {
+        private eTypeOfFuel m_TypeOfFuel;
+        private float m_AmountOfFuel;
+        private float m_MaxAmountOfFuel;
+
+
+
+        public eTypeOfFuel TypeOfFuel { get => m_TypeOfFuel; set => m_TypeOfFuel = value; }
+        public float AmountOfFuel { get => m_AmountOfFuel; set => m_AmountOfFuel = value; }
+        public float MaxAmountOfFuel { get => m_MaxAmountOfFuel; set => m_MaxAmountOfFuel = value; }
+
+        public void Refuel(float amountOfFuelToFill)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
